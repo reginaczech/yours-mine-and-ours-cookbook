aaf7aa574c34f2cfff5c7b6ebe4e48f3d1958c24
Regina Czech <regina.czech@yahoo.com>
2024-06-10 18:02:02 +0200

    client updated for vercel deployment

--- client/src/APIServices/fetchServices.js
+++ client/src/APIServices/fetchServices.js
@@ -1,34 +1,42 @@
-const rootURL = "http://localhost:3000";
+//local root
+// const rootURL = "http://localhost:3000";
+
+//vercel deployment url
+const rootURL = "https://yours-mine-and-ours-cookbook-my27.vercel.app/";
 
 //fetch service - get all recipes
 export const getAllRecipes = () => fetchGetRequest(`${rootURL}/recipes`);
 //fetch service - get recipe by id
-export const getRecipeById = (id) => fetchGetRequest(`${rootURL}/recipes/${id}`);
+export const getRecipeById = (id) =>
+  fetchGetRequest(`${rootURL}/recipes/${id}`);
 //fetch service - get categories
 export const getCategories = () => fetchGetRequest(`${rootURL}/categories`);
 //fetch service - get recipes by category
-export const getRecipesFromCategories = (id) => fetchGetRequest(`${rootURL}/categories/${id}`);
-
+export const getRecipesFromCategories = (id) =>
+  fetchGetRequest(`${rootURL}/categories/${id}`);
 
 //fetch service - post new recipe
-export const postNewRecipe = (data) => fetchPostRequest(`${rootURL}/add-new-recipe`, data);
+export const postNewRecipe = (data) =>
+  fetchPostRequest(`${rootURL}/add-new-recipe`, data);
 //fetch service - post url for webscraping and recieve recipe data
-export const postWebScrapingURL = (data) => fetchPostRequest(`${rootURL}/webscraped-recipe`, data);
+export const postWebScrapingURL = (data) =>
+  fetchPostRequest(`${rootURL}/webscraped-recipe`, data);
 //fetch service - post login
 export const postLogin = (data) => fetchPostRequest(`${rootURL}/login`, data);
 //fetch service - post login
-export const postRegister = (data) => fetchPostRequest(`${rootURL}/register`, data);
-
+export const postRegister = (data) =>
+  fetchPostRequest(`${rootURL}/register`, data);
 
 const fetchGetRequest = async (url) => {
   try {
-    const response = await fetch(url,
-    //   {
-    //   method: "GET",
-    //   credentials: "include",
-    //   mode: "cors",
-    //   headers: { "Content-Type": "application/json" },
-    // }
+    const response = await fetch(
+      url
+      //   {
+      //   method: "GET",
+      //   credentials: "include",
+      //   mode: "cors",
+      //   headers: { "Content-Type": "application/json" },
+      // }
     );
     const result = await response.json();
     return result;
@@ -36,7 +44,6 @@ const fetchGetRequest = async (url) => {
     console.log(`${error.message} while fetching /${url}`);
   }
 };
-
 
 const fetchPostRequest = async (url, data) => {
   try {
